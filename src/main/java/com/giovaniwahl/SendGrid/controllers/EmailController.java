08a22808c3ce41c700fe4492745c71469a70916a
Giovani Wahl <giovaniwahl@gmail.com>
2024-08-29 13:32:45 -0300

    EmailService

--- src/main/java/com/giovaniwahl/SendGrid/controllers/EmailController.java
+++ src/main/java/com/giovaniwahl/SendGrid/controllers/EmailController.java
@@ -1,6 +1,8 @@
 package com.giovaniwahl.SendGrid.controllers;
 
 import com.giovaniwahl.SendGrid.dto.EmailDto;
+import com.giovaniwahl.SendGrid.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,8 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/emails")
 public class EmailController {
+
+    private EmailService emailService;
+
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @PostMapping
     public ResponseEntity<Void> send(@RequestBody EmailDto dto){
+        emailService.sendEmail(dto);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
