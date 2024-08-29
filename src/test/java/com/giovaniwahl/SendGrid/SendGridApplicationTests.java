349ad588e38dd05aa0e11ff423e9747271dce98f
Giovani Wahl <giovaniwahl@gmail.com>
2024-08-29 11:05:33 -0300

    start

---
+++ src/test/java/com/giovaniwahl/SendGrid/SendGridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovaniwahl.SendGrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendGridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
